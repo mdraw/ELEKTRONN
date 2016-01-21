a73cf3a4b9b7877ca386f095136976d154735d53
mdraw <mdraw.gh@gmail.com>
2016-01-21 02:55:11 +0100

    :unamused:

--- test.py
+++ test.py
@@ -1,4 +1,9 @@
-from elektronn.net.convlayer2d import *
-from elektronn.training.warping import *
+#from elektronn.net.convlayer2d import *
+#from elektronn.training.warping import *
+from tqdm import trange
+from time import sleep
+
+for x in trange(40):
+    sleep(.1)
 
 assert 1 == 1
